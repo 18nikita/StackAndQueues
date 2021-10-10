bb93583974c61c9a41efa1c3c06d7b7eb4c501cc
unknown <rajputnikita5666@gmail.com>
2021-10-10 13:32:06 +0530

    Added:Create stack program

---
+++ src/main/java/com/bridgelabz/stackqueues/StackAndQueues.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.stackqueues;
+
+import java.util.Stack;
+
+public class StackAndQueues {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Stack and Queues");
+
+		Stack<Integer> stk = new Stack<Integer>();
+		stk.push(70);
+		stk.push(30);
+		stk.push(56);
+
+		System.out.println("Elements in Stack: " + stk);
+	}
+
+}
